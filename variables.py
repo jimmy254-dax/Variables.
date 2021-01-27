348d641601002136c732d79424ef4165d258ae42
jimmy254-dax <77729648+jimmy254-dax@users.noreply.github.com>
2021-01-27 17:03:03 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,32 @@
+print('Hello, world!')
+#varables
+#int
+score=80
+print(score)
+#float
+weight=23
+print(weight)
+#string
+x="pyhon variables"
+print(x)
+#integer assignment
+age=27
+#float salary
+salary=158000
+
+print(age)
+print(salary)
+a= b= c=30
+print(a)
+print(b)
+print(c
+a,b,c,=1,20.2, "python programming"
+
+print(a)
+print(b)
+print(c)
+a,b,c,=1,20.3, "python programming"
+
+print(a)
+print(b)
+print(c)
